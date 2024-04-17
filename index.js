ebd43b49971848cd5f2175efe567ca420905ceca
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-17 20:08:18 +0200

    Implemented the fetching function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 window.onload = function () {
-    fetch('GET', "https://api-consumers.onrender.com/")
-    .then(() => {window.location.href = "https://api-consumers.onrender.com/"})
+    const request = new XMLHttpRequest()
+    request.open('GET', 'https://api-consumers.onrender.com/')
+    request.setRequestHeader('Content-Type', 'application/json')
+    request.setRequestHeader('Accept', 'application/json')
+    request.setRequestHeader('Access-Control-Allow-Origin', '*')
+    request.addEventListener('load', function() {
+        if (request.status === 200 && request.readyState === 4) {
+            window.location.href = "https://api-consumers.onrender.com/"
+        } else {
+            if (request.status == 400 && request.readyState === 4) {
+                console.log(request.response)
+                // TODO: Add proper error handling
+            }
+        }
+    })
+    request.send()
 }
